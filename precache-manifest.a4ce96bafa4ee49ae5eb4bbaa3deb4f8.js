148b0aa2dcf25552491143775af7d102e99b7813
ewoken <ewokenfr@gmail.com>
2022-12-04 19:50:48 +0000

    Deploying to gh-pages from  @ 63c40edc2169c8232d63765e1544bd07f17ed1d1 🚀

---
+++ precache-manifest.a4ce96bafa4ee49ae5eb4bbaa3deb4f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e22ada966831c4e2dea23b5863b7754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b67eac36e98addba0cfd",
+    "url": "/static/css/2.76b7ca4d.chunk.css"
+  },
+  {
+    "revision": "b4e28948235c43ffc88b",
+    "url": "/static/css/main.d76f5a84.chunk.css"
+  },
+  {
+    "revision": "b67eac36e98addba0cfd",
+    "url": "/static/js/2.70cef6c7.chunk.js"
+  },
+  {
+    "revision": "cbf37cb886fb71a01abd9ffe5345579d",
+    "url": "/static/js/2.70cef6c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b4e28948235c43ffc88b",
+    "url": "/static/js/main.33787b58.chunk.js"
+  },
+  {
+    "revision": "e25be6e4e7f9ab8f9931",
+    "url": "/static/js/runtime-main.7bf31526.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30ae69ba0b2e844bdcb0e0fc19e919e3.js"
+  "/precache-manifest.a4ce96bafa4ee49ae5eb4bbaa3deb4f8.js"
 );
 
 self.addEventListener('message', (event) => {
